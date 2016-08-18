00af26fe7790a297a4de5fe60a20395762547668
pxai <pello_altadill@cuatrovientos.org>
2016-08-19 00:09:05 +0200

    ok, done changind dir name

--- src/middleware/dummyddleware.js
+++
@@ -1,9 +0,0 @@
-/**
-* dummyddleware.js
-* A dummy middleware just to see the order it is being called
-*/
-
-module.exports = function dummyddleware (req,res, next) {
-	console.log('Dummy middleware');
-	next();
-}
--- src/public/js/directives/directives.js
+++
@@ -1,9 +0,0 @@
-/**
-* directives.js
-* This defines a directives module, and will be used by all angular directives
-* Pello Xabier Altadill Izura - http://pello.io
-*/
-define(['angular'], function(angular) {
-	'use strict';
-	return angular.module('directives', []);
-});
